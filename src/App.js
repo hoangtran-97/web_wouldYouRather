d0114ca0ad7ecf37bb71e9134a6fc1400f6d49e3
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-05-25 22:18:31 +0300

    feat:rouating

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import {
+    BrowserRouter as Router, Switch, Route, Link
+} from 'react-router-dom';
+import {Home} from './pages/Home';
+import {NewQuestion} from './pages/NewQuestion';
+import {LeaderBoard} from './pages/LeaderBoard';
+import {Login} from './pages/Login';
 
 function App() {
     return (
-        <div className="App">
-            <div>Test</div>
-        </div>
+        <Router>
+            <Link to="/">Home</Link>
+            <Link to="/add">New Question</Link>
+            <Link to="/leaderboard">Leader Board</Link>
+            <Link to="/login">Login</Link>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/add">
+                    <NewQuestion />
+                </Route>
+                <Route path="/leaderboard">
+                    <LeaderBoard />
+                </Route>
+                <Route path="/login">
+                    <Login />
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
---
+++ src/pages/NewQuestion.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const NewQuestion = () => (
+    <div>
+        <p>Questions</p>
+    </div>
+);
