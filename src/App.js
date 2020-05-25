a8c5a7fc96be70c62612f1aa693b188b84ef6b71
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-05-25 22:34:24 +0300

    feat:store connected

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
     BrowserRouter as Router, Switch, Route, Link
 } from 'react-router-dom';
+import {connect} from 'react-redux';
 import {Home} from './pages/Home';
 import {NewQuestion} from './pages/NewQuestion';
 import {LeaderBoard} from './pages/LeaderBoard';
 import {Login} from './pages/Login';
+import {handleInitialData} from './actions/shared';
 
-function App() {
-    return (
-        <Router>
-            <Link to="/">Home</Link>
-            <Link to="/add">New Question</Link>
-            <Link to="/leaderboard">Leader Board</Link>
-            <Link to="/login">Login</Link>
-            <Switch>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route path="/add">
-                    <NewQuestion />
-                </Route>
-                <Route path="/leaderboard">
-                    <LeaderBoard />
-                </Route>
-                <Route path="/login">
-                    <Login />
-                </Route>
-            </Switch>
-        </Router>
-    );
+class App extends Component {
+    componentDidMount() {
+        this.props.dispatch(handleInitialData());
+    }
+
+    render() {
+        return (
+            <Router>
+                <Link to="/">Home</Link>
+                <Link to="/add">New Question</Link>
+                <Link to="/leaderboard">Leader Board</Link>
+                <Link to="/login">Login</Link>
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/add">
+                        <NewQuestion />
+                    </Route>
+                    <Route path="/leaderboard">
+                        <LeaderBoard />
+                    </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                </Switch>
+            </Router>
+        );
+    }
 }
 
-export default App;
+function mapStateToProps({authedUser}) {
+    return {loading: authedUser === null};
+}
+export default connect(mapStateToProps)(App);
---
+++ src/actions/shared.js
@@ -0,0 +1,10 @@
+import {getInitialData} from '../utils/api';
+import {receiveUsers} from './users';
+
+import {setAuthedUser} from './authedUser';
+
+const AUTHED_ID = 'tylermcginnis';
+export const handleInitialData = () => (dispatch) => getInitialData().then(({users}) => {
+    dispatch(receiveUsers(users));
+    dispatch(setAuthedUser(AUTHED_ID));
+});
