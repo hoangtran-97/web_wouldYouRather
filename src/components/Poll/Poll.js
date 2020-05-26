20fd3d8f74efbb5e7a62de2af143763556fd21e7
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-05-26 17:06:03 +0300

    feat: show voted

--- src/components/Poll/Poll.js
+++ src/components/Poll/Poll.js
@@ -4,43 +4,38 @@ import {Redirect} from 'react-router-dom';
 import './style.css';
 
 class Poll extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {votedOne: false, votedTwo: false};
-    }
-
-    componentDidMount() {}
-
     render() {
         const {
             users, id, questions, authedUser
         } = this.props;
+
         if (questions[id] === undefined) {
             return <p>This Poll does not exist</p>;
+        }
+        if (authedUser === null) {
+            alert('You need to login first');
+            return <Redirect to="/login" />;
         }
         const {author, optionOne, optionTwo} = questions[id];
         const {avatarURL} = users[authedUser];
         const totalVotes = optionOne.votes.length + optionTwo.votes.length;
         const onePercent = ((optionOne.votes.length / totalVotes) * 100).toFixed(0);
         const twoPercent = 100 - onePercent;
-        if (optionOne.votes.includes(authedUser)) {
-            console.log('voted 1');
-        }
-        if (optionTwo.votes.includes(authedUser)) {
-            console.log('voted 2');
-        }
-        const {votedOne, votedTwo} = this.state;
+        const styles = {
+            hightlight1: {border: optionOne.votes.includes(authedUser) && '1px solid green'},
+            hightlight2: {border: optionTwo.votes.includes(authedUser) && '1px solid green'}
+        };
         return (
             <div className="poll_container">
                 <h1>Would you rather</h1>
                 <img src={avatarURL} alt="ava" className="login_user_ava" />
                 <p>Author: {author}</p>
-                <div className="poll_vote_button" style={{border: votedOne && '1px solid green'}}>
+                <div className="poll_vote_button" style={styles.hightlight1}>
                     <button type="button"> {optionOne.text}</button>
                     <div className="vote_score">{optionOne.votes.length}</div>
                     <div className="vote_percent">{onePercent}%</div>
                 </div>
-                <div className="poll_vote_button">
+                <div className="poll_vote_button" style={styles.hightlight2}>
                     <button type="button"> {optionTwo.text}</button>
                     <div className="vote_score">{optionTwo.votes.length}</div>
                     <div className="vote_percent">{twoPercent}%</div>
