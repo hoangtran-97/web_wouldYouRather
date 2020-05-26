328be7582fd652b6485e1cde9d746611ae0ae343
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-05-26 11:21:34 +0300

    feat: logout

--- src/components/LoginUser/LoginUser.js
+++ src/components/LoginUser/LoginUser.js
@@ -1,17 +1,28 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Redirect} from 'react-router-dom';
 import './style.css';
 import {setAuthedUser} from '../../actions/authedUser';
 
 class LoginUser extends Component {
-    componentDidMount() {}
+    constructor(props) {
+        super(props);
+        this.state = {toHome: false};
+    }
 
     render() {
         const {users, id, dispatch} = this.props;
+        const {toHome} = this.state;
         const {avatarURL, name} = users[id];
         const login = () => {
             dispatch(setAuthedUser(id));
+            this.setState(() => ({
+                toHome: true
+            }));
         };
+        if (toHome === true) {
+            return <Redirect to="/" />;
+        }
         return (
             <button type="button" className="login_user_container" onClick={() => login()}>
                 <p>{name}</p>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+
 import LoginUser from '../components/LoginUser/LoginUser';
 
 class Login extends Component {
